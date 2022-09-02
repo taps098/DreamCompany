d84ea2d3ecfdec644ab04ac042661accd66bae07
Taps B <63951156+taps098@users.noreply.github.com>
2022-09-02 15:04:15 +0200

    table intitial commits

---
+++ src/components/TableComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const TableComponent = () => {
+    return (
+        <table>
+            <tr>
+                <th>Company Name</th>
+                <th>Application Status</th>
+                <th>Interview Status</th>
+                <th>Job Location</th>
+                <th>Actions</th>
+            </tr>
+        </table>
+    )
+};
+
+export default TableComponent;
